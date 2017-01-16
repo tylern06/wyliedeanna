ef5f5ccb6c8a9a93d5e72aeccec0cbe161468988
Tyler Nguyen <tynguyen06@gmail.com>
2017-01-16 10:42:48 -0800

    first commit

---
+++ client/js/scripts.js
@@ -0,0 +1,268 @@
+$(document).ready(function() {
+
+    // "use strict";
+
+    // Smooth scroll to inner links
+
+    $('.inner-link').smoothScroll({
+        offset: -59,
+        speed: 800
+    });
+
+    //initiate lightgallery
+    // $("#lightgallery").lightGallery(); 
+
+
+    // Add scrolled class to nav
+
+    // $(window).scroll(function() {
+    //     if ($(window).scrollTop() > 0) {
+    //         $('nav').addClass('scrolled');
+    //     } else {
+    //         $('nav').removeClass('scrolled');
+    //     }
+    // });
+
+    // Set nav container height for fixed nav
+
+    if (!$('nav').hasClass('transparent')) {
+        $('.nav-container').css('min-height', $('nav').outerHeight());
+    }
+
+    // Mobile toggle
+
+    $('.mobile-toggle').click(function() {
+        $('nav').toggleClass('nav-open');
+    });
+
+    $('.menu li a').click(function() {
+        if ($(this).closest('nav').hasClass('nav-open')) {
+            $(this).closest('nav').removeClass('nav-open');
+        }
+    });
+
+    // TweenMAX Scrolling override on Windows for a smoother experience
+
+    if (navigator.appVersion.indexOf("Win") != -1) {
+        if (navigator.userAgent.toLowerCase().indexOf('chrome') > -1) {
+            $(function() {
+
+                var $window = $(window);
+                var scrollTime = 0.4;
+                var scrollDistance = 350;
+
+                $window.on("mousewheel DOMMouseScroll", function(event) {
+
+                    event.preventDefault();
+
+                    var delta = event.originalEvent.wheelDelta / 120 || -event.originalEvent.detail / 3;
+                    var scrollTop = $window.scrollTop();
+                    var finalScroll = scrollTop - parseInt(delta * scrollDistance);
+
+                    TweenMax.to($window, scrollTime, {
+                        scrollTo: {
+                            y: finalScroll,
+                            autoKill: true
+                        },
+                        ease: Power1.easeOut,
+                        overwrite: 5
+                    });
+
+                });
+            });
+        }
+    }
+
+    // Append .background-image-holder <img>'s as CSS backgrounds
+
+    $('.background-image-holder').each(function() {
+        var imgSrc = $(this).children('img').attr('src');
+        $(this).css('background', 'url("' + imgSrc + '")');
+        $(this).children('img').hide();
+        $(this).css('background-position', '50% 50%');
+    });
+
+    // Fade in background images
+
+    setTimeout(function() {
+        $('.background-image-holder').each(function() {
+            $(this).addClass('fadeIn');
+        });
+        $('.header.fadeContent').each(function() {
+            $(this).addClass('fadeIn');
+        });
+    }, 200);
+
+
+    // Parallax scrolling
+
+    if (!(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)) {
+        if (window.requestAnimationFrame) {
+            parallaxBackground();
+            $(window).scroll(function() {
+                requestAnimationFrame(parallaxBackground);
+            });
+        }
+    } else {
+        $('.parallax').removeClass('parallax');
+    }
+
+    // Image fade on story 2 element
+
+    $('.story-2 img').mouseenter(function() {
+        $(this).removeClass('fade');
+        $(this).siblings().addClass('fade');
+    });
+
+    $('.story-2 img').mouseleave(function() {
+        $(this).closest('.row').find('img').removeClass('fade');
+    });
+
+
+    // Sliders
+
+    $('.image-slider').flexslider({
+        directionNav: false
+    });
+
+    // Radio box controls
+
+    $('.radio-holder').click(function() {
+        $(this).siblings().find('input').prop('checked', false);
+        $(this).find('input').prop('checked', true);
+        $(this).closest('.radio-group').find('.radio-holder').removeClass('checked');
+        $(this).addClass('checked');
+    });
+
+    $('form input[type="radio"]').each(function() {
+        var valueText = $(this).closest('.radio-holder').find('span').text();
+        // $(this).attr('value', convertToSlug(valueText));
+    });
+
+    $('form input[type="text"]').each(function() {
+        var attrText = $(this).attr('placeholder');
+        // $(this).attr('name', convertToSlug(attrText));
+    });
+
+
+    // Instagram Feed
+
+    jQuery.fn.spectragram.accessData = {
+        accessToken: '12979932440.1677ed0.05361c32b6244885a4abe594804babb4',
+        clientID: '864bfd85db22407b864bcc1e3bf7388b'
+    };
+
+    // $('.instafeed').each(function() {
+    //     $(this).children('ul').spectragram('getUserFeed', {
+    //         // console.log('hello');
+    //         query: $(this).attr('data-user-name')
+    //     });
+    // });
+$('#insane').spectragram('getPopular');
+
+
+
+    $('.validate-required, .validate-email').on('blur change', function() {
+        validateFields($(this).closest('form'));
+    });
+
+    $('form').each(function() {
+        if ($(this).find('.form-error').length) {
+            $(this).attr('original-error', $(this).find('.form-error').text());
+        }
+    });
+
+    function validateFields(form) {
+        var name, error, originalErrorMessage;
+
+        $(form).find('.validate-required[type="checkbox"]').each(function() {
+            if (!$('[name="' + $(this).attr('name') + '"]:checked').length) {
+                error = 1;
+                name = $(this).attr('name').replace('[]', '');
+                form.find('.form-error').text('Please tick at least one ' + name + ' box.');
+            }
+        });
+
+        $(form).find('.validate-required').each(function() {
+            if ($(this).val() === '') {
+                $(this).addClass('field-error');
+                error = 1;
+            } else {
+                $(this).removeClass('field-error');
+            }
+        });
+
+        $(form).find('.validate-email').each(function() {
+            if (!(/(.+)@(.+){2,}\.(.+){2,}/.test($(this).val()))) {
+                $(this).addClass('field-error');
+                error = 1;
+            } else {
+                $(this).removeClass('field-error');
+            }
+        });
+
+        if (!form.find('.field-error').length) {
+            form.find('.form-error').fadeOut(1000);
+        }
+
+        return error;
+    }
+
+});
+
+$(window).load(function() {
+
+    // Append Instagram BGs
+
+    var setUpInstagram = setInterval(function() {
+        if ($('.instafeed li').hasClass('bg-added')) {
+            clearInterval(setUpInstagram);
+            return;
+        } else {
+            $('.instafeed li').each(function() {
+
+                // Append background-image <img>'s as li item CSS background for better responsive performance
+                var imgSrc = $(this).find('img').attr('src');
+                $(this).css('background', 'url("' + imgSrc + '")');
+                $(this).find('img').css('opacity', 0);
+                $(this).css('background-position', '50% 0%');
+                // Check if the slider has a color scheme attached, if so, apply it to the slider nav
+                $(this).addClass('bg-added');
+            });
+            $('.instafeed').addClass('fadeIn');
+        }
+    }, 500);
+
+});
+
+// function convertToSlug(text) {
+//     return text
+//         .toLowerCase()
+//         .replace(/[^\w ]+/g, '')
+//         .replace(/ +/g, '-');
+// }
+
+function parallaxBackground() {
+    $('.parallax').each(function() {
+        var element = $(this);
+        var scrollTop = $(window).scrollTop();
+        var scrollBottom = scrollTop + $(window).height();
+        var elemTop = element.offset().top;
+        var elemBottom = elemTop + element.outerHeight();
+
+        if ((scrollBottom > elemTop) && (scrollTop < elemBottom)) {
+            if (element.is('section:first-of-type')) {
+                var value = (scrollTop / 7);
+                $(element).find('.background-image-holder').css({
+                    transform: 'translateY(' + value + 'px)'
+                });
+            } else {
+                var value = ((scrollBottom - elemTop) / 7);
+                $(element).find('.background-image-holder').css({
+                    transform: 'translateY(' + value + 'px)'
+                });
+            }
+
+        }
+    });
+}
